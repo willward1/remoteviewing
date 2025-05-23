e6d5199e2cecf94033fd93bb51995077a69164d0
willward1 <willwardwillward@gmail.com>
2025-05-22 21:08:08 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import RandomLocationGenerator from './RandomLocationGenerator';
+import RandomLocationGenerator from './randomlocationgenerator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
